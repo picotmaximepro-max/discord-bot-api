5a60f1785fede6257fd0406a8b420d0ea927bb7d
picotmaximepro-max <picotmaxime.pro@gmail.com>
2025-09-19 11:56:01 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const { Client, GatewayIntentBits } = require('discord.js');
-const cors = require('cors');
+import express from 'express';
+import { Client, GatewayIntentBits } from 'discord.js';
+import cors from 'cors';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
